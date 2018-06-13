1dcb244b3c1d30c68c0df2bb6c39573df6b54667
Rodrigo Filomeno <rodrigo.filomeno@al.infnet.edu.br>
2018-06-13 11:32:13 -0300

    implementando logica de sorteio

--- app/src/main/java/com/example/victordias/sempanela/Model/Sorteio.java
+++ app/src/main/java/com/example/victordias/sempanela/Model/Sorteio.java
@@ -1,6 +1,7 @@
 package com.example.victordias.sempanela.Model;
 
 import java.util.List;
+import java.util.Random;
 
 public class Sorteio {
 
@@ -14,6 +15,9 @@ public class Sorteio {
         //sempre deixando os times com o maximo de jogadores possivel
         int qtdJogadoresPorTime = jogadores.size() / qtTimes;
 
+        int sorteado = 0;
+        Random random = new Random();
+
         //laço que define o tamanho dos times e a quantidade de jogadores na reserva
         while((jogadores.size() + numeroDeReservas) % qtTimes != 0){
             numeroDeReservas++;
@@ -22,8 +26,11 @@ public class Sorteio {
         for(int i = 0; i<qtTimes; i++){
             Time time = new Time();
             for(int z = 0; z < qtdJogadoresPorTime; z++){
-                //logica para sorteio
+                sorteado = random.nextInt(jogadores.size());
+                time.setJogador(jogadores.get(sorteado));
+                jogadores.remove(jogadores.get(sorteado));
             }
+            times.add(time);
         }
 
         //Se houver mais jogadores do que o tamanho dos
@@ -41,7 +48,7 @@ public class Sorteio {
     //Realiza o sorteio de times levando em consideração a quantidade de jogadores x time
     public List<Time> realizarSorteioQtJogadoresPorTime(List<Jogador> jogadores, int qtJDxTM){
 
-        if(qtJDxTM > jogadores.size()){
+        if(qtJDxTM > jogadores.size() && qtJDxTM <= 0){
             return null;
         }
         int qtTimes = jogadores.size() / qtJDxTM;
