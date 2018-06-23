75213aff58b169b333eb486ab573ec88d2eec83e
victorhugodias <victor.dias@al.infnet.edu.br>
2018-06-23 13:51:56 -0300

    monetização

--- app/src/main/java/com/example/victordias/sempanela/Controller/MainActivity.java
+++ app/src/main/java/com/example/victordias/sempanela/Controller/MainActivity.java
@@ -6,16 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.victordias.sempanela.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MobileAds.initialize(this,
+                "ca-app-pub-2048265031968487/2133458539");
 
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
     }
 
